fe66e0240a9739cc244b6b6b6903eca25ecce456
wynhsu <wynhsu@uw.edu>
2016-10-06 14:35:41 -0700

    function

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -13,3 +13,16 @@
 
 
 # Create a variable `height.in.meters` by passing `height.in.feet` to your `FeetToMeters` function
+
+AddThree <- function(a) {
+  answer <- a + 3
+  return (answer)
+}
+ten <- AddThree(7)
+
+FeetToMeters <- function(ft) {
+  answer <- ft * 0.3048
+  return (answer)
+}
+height.in.feet <- 5.92
+height.in.meters <- FeetToMeters(height.in.feet)
